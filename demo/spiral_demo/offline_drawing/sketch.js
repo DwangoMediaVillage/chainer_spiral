8a36873ee3ab3aa2b9fae809bc2ed308efe39955
Kazuma Sasaki <kazuma_sasaki@dwango.co.jp>
2018-10-02 15:18:41 +0900

    multiple drawing space

--- demo/spiral_demo/offline_drawing/sketch.js
+++ demo/spiral_demo/offline_drawing/sketch.js
@@ -1,19 +1,20 @@
-var canvasWidth = 400
-var canvasHeight = 400
-var bg_color = 245
-var cursor_size = 20
-var stroke_weight_ratio = 10
-var frame_rate = 5
 var jsonfile = 'actions.json'
+var bg_color = 245
+var cursor_size = 4
+var stroke_weight_ratio = 5.0
+var border_weight = 3
+var graphics_size = 100
+var frame_rate = 10
+var n_rows = 5
+var n_cols = 5
 
 var canvas
-var played = false
-var action = []
-var prev_x = 0
-var prev_y = 0
 var data
-var step
-var step_length
+var graphics = []
+var t = 0
+var actions = []
+var steps = []
+var played = false
 
 function preload() {
 	console.log('Loading ' + jsonfile)
@@ -24,92 +25,141 @@ function randint(n) {
 	return Math.floor(Math.random() * Math.floor(n))
 }
 
-function sample_action() {
-	var i = randint(data['actions'].length)
-	var sampled_data = data['actions'][i].slice()
-
-	// reset the timer
-	step = 0
-
-	return sampled_data
-}
-
 function setup() {
-	canvas = createCanvas(canvasWidth, canvasHeight);
+
+	graphics_size = graphics_size + border_weight * 2
+
+	canvas = createCanvas(graphics_size * n_rows, graphics_size * n_cols)
 	canvas.parent('sketch-holder')
 	frameRate(frame_rate)
-}
 
-function play() {
-	// clear canvas
-	clear()
-	action = sample_action()
-	step_length = action.length
-	played = true
-}
-
-
-function draw() {
-	// draw background
-	background(bg_color)
-
-	// draw border of the canvas
-	stroke(0)
-	strokeWeight(1)
-	noFill()
-	rect(0, 0, canvasWidth-1, canvasHeight-1)
-
-	// draw contents
-	if (played) {
-		drawAction(action, step)
-		step += 1
-		if (step == step_length) {
-			// reaches the terminal state. stop playing
-			played = false
+	// set graphics
+	for (var i=0; i<n_rows; i++) {
+		for (var j=0; j<n_cols; j++){
+			var g = createGraphics(graphics_size, graphics_size)
+			init_graphic(g)
+			image(g, i * graphics_size, j * graphics_size)
+			graphics.push(g)
 		}
 	}
 }
 
-function drawAction(action, step) {
-	// draw a cursor at the latest action
-	if (step > 0) {
-		drawCursor(action[step-1], 128)
+
+function draw() {
+	background(255);
+	var n = 0
+	var stopped = 0
+	for (var i=0; i<n_rows; i++) {
+		for (var j=0; j<n_cols; j++){
+			var g = graphics[n_cols * i + j]
+
+			// update graphic
+			if (played) {
+				// draw background and border
+				init_graphic(g)
+
+				// update graphic
+				stopped += update_graphic(g, actions[n], steps[n], t)
+			}
+			
+			// render graphics to the canvas
+			image(g, i * graphics_size, j * graphics_size)
+			n += 1
+		}
 	}
 
-	drawCursor(action[step], 255)
+	// update time counter
+	t += 1
 
-	// draw sequence of lines until the indicated time step
-	for (var t = 0; t < step; t++){
-		drawLine(action[t])
+	// if all the graphics finish to draw, stop playing
+	if (stopped == n) {
+		console.log('playing stopped')
+		played = false
 	}
+
 }
 
-function drawCursor(action, alpha) {
-	var [x, y, p, r, g, b, q] = action
+function play() {
+	// sample action sequences from json buffer, and starts playing
+	clear()
+	actions = []
+	steps = []
 
-	x = x * canvasWidth
-	y = y * canvasHeight
+	t = 0
+	for (var i=0; i < n_rows * n_cols; i++) {
+		var action = sample_action()
+		actions.push(action)
+		steps.push(action.length)
+	}
 
-	// draw cursor
-	stroke(255,105,180, alpha)  // hot pink
-	strokeWeight(4)
-	ellipse(x, y, cursor_size, cursor_size)
+	played = true
 }
 
-function drawLine(action) {
-	var [x, y, p, r, g, b, q] = action
+function sample_action() {
+	var i = randint(data['actions'].length)
+	return data['actions'][i].slice()
+}
 
-	x = x * canvasWidth
-	y = y * canvasHeight
+function init_graphic(graphic) {
+	// draw background and border of a grahics
+	graphic.background(bg_color)
 
-	if (q) {
-		// draw line
-		strokeWeight(p * stroke_weight_ratio)
-		stroke(r * 255, g * 255, b * 255)
-		line(prev_x, prev_y, x, y)
-	}
+	// draw border of the canvas
+	graphic.stroke(255)
+	graphic.strokeWeight(border_weight)
+	graphic.noFill()
+	graphic.rect(0, 0, graphics_size-border_weight, graphics_size-border_weight)
+}
+
+function update_graphic(graphic, action, steps, t) {
+	// may be update a graphic, and returns 1 if step t reaches the terminal state
 	
-	// update the previous point
-	prev_x = x
-	prev_y = y
+	// draw cursor
+	if (t > 0) {
+		drawCursor(graphic, action[t-1], 128)
+	}
+	drawCursor(graphic, action[t], 255)
+
+	var t_terminate = 0
+	if (t >= steps - 1) {
+		t_terminate = steps - 1
+	} else {
+		t_terminate = t
+	}
+
+	// draw lines
+	drawLines(graphic, action, t_terminate)
+
+	return t >= steps - 1
+}
+
+
+function drawCursor(graphic, action, alpha) {
+	var [x, y, p, r, g, b, q] = action
+	x = scale_point(x)
+	y = scale_point(y)
+	graphic.stroke(255, 105, 180, alpha)
+	graphic.strokeWeight(4)
+	graphic.ellipse(x, y, cursor_size, cursor_size)
+}
+
+function drawLines(graphic, action, t_terminate) {
+	var prev_x = 0
+	var prev_y = 0
+	for (var t = 0; t < t_terminate; t++) {
+		var [x, y, p, r, g, b, q] = action[t]
+		x = scale_point(x)
+		y = scale_point(y)
+		if (q) {
+			graphic.strokeWeight(p * stroke_weight_ratio)
+			graphic.stroke(r * 255, g * 255, b * 255)
+			graphic.line(prev_x, prev_y, x, y)
+		}
+		prev_x = x
+		prev_y = y
+	}
+}
+
+function scale_point(x) {
+	return x * (graphics_size - border_weight * 2) + border_weight
 }
