b1ba05a4cd0f7c2808e02e75ab34a2ba24ecc299
Kazuma Sasaki <kazuma_sasaki@dwango.co.jp>
2019-02-04 14:47:53 +0900

    fix plot_scores

--- plot_scores.py
+++ plot_scores.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-import os
+from pathlib import Path
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
 import pandas
 import math
 import matplotlib.gridspec as gridspec
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 EXCEPT_TAGS = ('steps',
                 'episodes',
@@ -19,58 +19,47 @@ EXCEPT_TAGS = ('steps',
                 'max',
                 'min')
 
-def set_axis_prop(ax, title, x_label):
+def load_scores(score_dir):
+    logging.info(f"load scores.txt from {score_dir}")
+    assert (score_dir / 'scores.txt').exists(), 'scores.txt does not exist!'
+
+    df = pandas.read_csv(score_dir / 'scores.txt', 
+                                dtype=np.float32, 
+                                na_values='None',
+                                sep='\t').sort_values('steps')
+  
+    df['elapsed'] = pandas.to_timedelta(df['elapsed'], unit='s')
+    df = df.set_index('elapsed')
+
+    # resample logs as every hour
+    df = df.resample('1H').mean()
+
+    return df
+ 
+
+def plot_val(ax, steps, val, title, color='royalblue'):
+    ax.plot(steps, val, color=color)
     ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.grid()
+    ax.set_xlabel('Step [k]')
+    ax.grid(True)
+
 
 def plot_score(args):
     logger.debug('plot scores.txt in %s', args.target_dir)
 
-    target = os.path.join(args.target_dir, 'scores.txt')
+    data = load_scores(args.target_dir)
 
-    assert os.path.exists(target)
-
-    table = pandas.read_table(target, dtype=np.float32, na_values='None')
-
-    steps = np.array(table['steps'])
-
-    # fix ordering of the records due to a bug
-    inds = np.argsort(steps)
-    steps = steps[inds]
-
-    elapsed = np.array(table['elapsed'])[inds]
-
-    if steps[-1] > 1000:
-        elapsed_min = elapsed[-1] / steps[-1] * 1000
-        elapsed_min = elapsed_min / 60 
-        print(f" elapsed per 1k steps [min] = {elapsed_min}")
-
-    cut_idx = None
-
-    data = {}
-    for col in table.columns:
-        if not col in EXCEPT_TAGS:
-            data[col] = np.array(table[col])[inds]
-            is_nan_idx = np.where(np.isnan(data[col]))[0]
-            if len(is_nan_idx) and is_nan_idx.min() > 0:
-                cut_idx = is_nan_idx.min()
-    
-    if cut_idx:
-        steps = steps[:cut_idx]
-        elapsed = steps[:cut_idx]
-
-    logger.info(f"num_log per step = {1000 * len(steps) / steps[-1]}")
+    logger.info(f"steps per 1 hour = {data['steps'][1] - data['steps'][0]}")
 
     # fix order
-    if steps.max() > 100000:
-        steps = steps / 1000
+    if data['steps'].max() > 100000:
+        data['steps'] = data['steps'] / 1000
         x_label = 'Step [k]'
     else:
         x_label = 'Step'
 
     # number of plots
-    N = len(data.keys())
+    N = len(data.keys()) - len(EXCEPT_TAGS)
     n_cols = 3
     n_rows = int(math.ceil(N / n_cols))
     
@@ -79,21 +68,10 @@ def plot_score(args):
     gs = gridspec.GridSpec(n_rows, n_cols)
 
     n = 0
-    for key, value in data.items():
+    for col in data.columns:
+        if col in EXCEPT_TAGS: continue 
         ax = plt.subplot(gs[n])
-
-        scalar = value[inds]
-        if cut_idx:
-            scalar = value[inds][:cut_idx]
-        
-        if args.conv_num:
-            num = args.conv_num
-            b = np.ones(num) / num
-            scalar = np.convolve(scalar, b, mode='same')
-
-        ax.plot(steps, scalar)
-        set_axis_prop(ax, key, x_label)
-        ax.set_xlim(0, steps.max())
+        plot_val(ax, data['steps'], data[col], col)
         n += 1
 
     # plt.suptitle(target)
@@ -103,9 +81,8 @@ def plot_score(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('target_dir')
+    parser.add_argument('target_dir', type=Path)
     parser.add_argument('savename')
-    parser.add_argument('--conv_num', type=int)
     args = parser.parse_args()
 
     plot_score(args)
